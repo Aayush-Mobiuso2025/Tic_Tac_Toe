e2e2c0329801a2d61b90db5bf2e302e9ec022724
Aayush-Mobiuso2025 <Aayush.kumbhar@mobiuso.com>
2025-01-19 12:43:11 +0530

    Update Sqlite Exercise.sql Q3 albums by artists with the word ‘black’ in their name.

--- Sqlite Exercise.sql
+++ Sqlite Exercise.sql
@@ -7,3 +7,8 @@ WHERE AlbumId = 67;
 SELECT Name, Milliseconds / 1000 AS LengthInSeconds 
 FROM tracks 
 WHERE LengthInSeconds BETWEEN 50 AND 70;
+
+SELECT albums.Title 
+FROM albums
+JOIN artists ON albums.ArtistId = artists.ArtistId 
+WHERE artists.Name LIKE '%black%';
