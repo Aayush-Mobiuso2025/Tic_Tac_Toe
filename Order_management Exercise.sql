ecc12572784f146ba862442f016b43886dc02ebc
Aayush-Mobiuso2025 <Aayush.kumbhar@mobiuso.com>
2025-01-20 19:58:06 +0530

    Update Order_management Exercise.sql Q3 Count of cities other than USA & MALAYSIA country with more than 1 city.

--- Order_management Exercise.sql
+++ Order_management Exercise.sql
@@ -18,5 +18,15 @@ select * from product;
 select * from product_class;
 select * from shipper;
 
+-- Q 3 Query to Show the count of cities in all countries other than USA & MALAYSIA, with more than 1 city, in the descending order of CITIES.
+
+SELECT 
+    address.country AS country, 
+    COUNT(address.city) AS city_count
+FROM address
+WHERE address.country NOT IN ('USA', 'Malaysia')
+GROUP BY address.country
+HAVING COUNT(address.city) > 1
+ORDER BY city_count DESC;
 
 
