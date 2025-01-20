314e01841dc9ce2bdd6a8d9b81490695779abe43
Aayush-Mobiuso2025 <Aayush.kumbhar@mobiuso.com>
2025-01-20 19:33:38 +0530

    Add Order_management Exercise file ( set up of database)

---
+++ Order_management Exercise.sql
@@ -0,0 +1,22 @@
+USE orders;
+SHOW TABLES;
+
+DESCRIBE product;
+DESCRIBE carton;
+DESCRIBE order_items;
+DESCRIBE order_header;
+DESCRIBE address;
+DESCRIBE product_class;
+
+select * from product;
+select * from address;
+select * from carton;
+select * from online_customer;
+select * from order_header;
+select * from order_items;
+select * from product;
+select * from product_class;
+select * from shipper;
+
+
+
