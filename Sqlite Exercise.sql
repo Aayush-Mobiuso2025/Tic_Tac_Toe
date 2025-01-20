42f021277eb6ac727d64a52442faef1ddabc8375
Aayush-Mobiuso2025 <Aayush.kumbhar@mobiuso.com>
2025-01-20 11:55:42 +0530

    Update Sqlite Exercise.sql Q10 Purchased track of 2013.

--- Sqlite Exercise.sql
+++ Sqlite Exercise.sql
@@ -122,6 +122,11 @@ GROUP BY media_types.MediaTypeId
 ORDER BY PurchaseCount DESC 
 LIMIT 1;
 
-
+SELECT tracks.Name AS TrackName, COUNT(invoice_items.Quantity) AS UnitsSold 
+FROM tracks
+JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId 
+JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId 
+WHERE strftime('%Y', invoices.InvoiceDate) = '2013' 
+GROUP BY tracks.TrackId;
 
 
