5e74fe87743312258d289e5284d57697356c8511
Aayush-Mobiuso2025 <Aayush.kumbhar@mobiuso.com>
2025-02-20 23:22:26 +0530

    Update board.py (add function fire_at, game_over)

--- models/board.py
+++ models/board.py
@@ -14,3 +14,16 @@ class Board:   #Handles board representation, ship placement, and firing mechani
             self.ships += 1
         else:
             raise ValueError("Ship already placed here.")
+
+    def fire_at(self, row, col):  #Handles firing at a given coordinate.
+        if self.board[row][col] == 'S':
+            self.board[row][col] = 'H'
+            self.hits += 1
+            return "Hit"
+        elif self.board[row][col] == ' ':
+            self.board[row][col] = 'M'
+            return "Miss"
+        return "Already fired"
+
+    def is_game_over(self):  #Checks if all ships have been hit.
+        return self.hits == self.ships
