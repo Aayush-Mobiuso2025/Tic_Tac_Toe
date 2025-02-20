044354a51e70d391f05eb01d62fd703a93bded36
Aayush-Mobiuso2025 <Aayush.kumbhar@mobiuso.com>
2025-02-20 23:48:20 +0530

    Update messages.py (Created a class message)

--- config/messages.py
+++ config/messages.py
@@ -1,4 +1,11 @@
-
+class Messages:  #Stores all messages used in the game.
+    PLACE_SHIPS = "Player 1: Place your ships ({} remaining)."
+    ALL_SHIPS_PLACED = "All ships placed! Press 'Change Turn' to continue."
+    PLAYER2_FIRE = "Player 2: Fire at Player 1's board."
+    HIT = "Hit!"
+    MISS = "Miss!"
+    ALREADY_FIRED = "Already fired at this location."
+    GAME_OVER = "Game Over! Player 2 Wins!"
 
 
 
